4e5fb4d1e81423dfab08abf2c0c65800daa8493e
sturdyplum <pabbelt@gmail.com>
2017-09-06 00:09:55 -0400

    Update DisjointSet.cpp

--- Code/DataStructures/DisjointSet.cpp
+++ Code/DataStructures/DisjointSet.cpp
@@ -1,4 +1,8 @@
 /*
+    Not Tested!
+*/
+
+/*
  Disjoing set amortized order one operations
 */
 
@@ -12,18 +16,17 @@ void merge(vector<int> &C, int x, int y) { C[find(C, x)] = find(C, y); }
 
 using namespace std;
 
-class UnionFind
+struct UnionFind
 {
-  private: vector<int> p, r ,s;
-  public:
+    vector<int> p, r ,s;
     int numDisjoint;
     UnionFind(int N)
-    {
-      numDisjoint = N;
-      r.assign(N, 0);
-      s.assign(N,1);// this is only needed if you are going to be querying this
-      p.assign(N, 0);
-      for (int i = 0; i < N; i++) p[i] = i;
+    { 
+        numDisjoint = N;
+        r.assign(N, 0);
+        s.assign(N,1);// this is only needed if you are going to be querying this
+        p.assign(N, 0);
+        for (int i = 0; i < N; i++) p[i] = i;
     }
 
     int findSet(int i) { return (p[i] == i) ? i : (p[i] = findSet(p[i])); }
@@ -32,21 +35,21 @@ class UnionFind
 
     void unionSet(int i, int j)
     {
-      if (!isSameSet(i, j)) // if from different set
-      {
-        numDisjoint--;
-        int x = findSet(i), y = findSet(j);
-        if (r[x] > r[y])
- {
-   p[y] = x; // rank keeps the tree short
-   s[x] += s[y];
- }
-        else {
-          p[x] = y;
-          if (r[x] == r[y]) r[y]++;
- 	   s[y] += s[x];
+        if (!isSameSet(i, j)) // if from different set
+        {
+            numDisjoint--;
+            int x = findSet(i), y = findSet(j);
+            if (r[x] > r[y])
+            {
+                p[y] = x; // rank keeps the tree short
+                s[x] += s[y];
+            }
+            else {
+                p[x] = y;
+                if (r[x] == r[y]) r[y]++;
+                s[y] += s[x];
+            }
         }
-      }
     }
 
     int sizeOfSet(int i) {return s[findSet(i)];}
