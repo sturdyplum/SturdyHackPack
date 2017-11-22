4dfcef6c47a2bace9add2d68a00144caf9490bef
sturdyplum <pabbelt@gmail.com>
2017-11-22 00:47:09 -0500

    Update Util.java

--- Java/Geometry/Util.java
+++ Java/Geometry/Util.java
@@ -1,42 +1,56 @@
 package Geom;
-import java.util.ArrayList;
+import java.util.Arrays;
 
+public class Util{
 
-public class Polygon extends Util{
-
-	ArrayList<Point> p = new ArrayList<>();
-	int n;
-
-	Polygon(ArrayList<Point> _p) {
-		p = _p;
-		n = p.size();
+	// returns dot product of |a-b|*|b-c|
+	public static double dot(Point a, Point b, Point c) {
+		Point ab = new Point(b.x - a.x, b.y - a.y);
+		Point bc = new Point(c.x - b.x, c.y - b.y);
+		return ab.x * bc.x + ab.y * bc.y;
 	}
 
-	int prev(int i) {
-		return i == 0 ? n - 1 : i - 1;
+	// return cross product of |b-a|x|c-a|
+	public static double cross(Point a, Point b, Point c) {
+		Point ab = new Point(b.x - a.x, b.y - a.y);
+		Point ac = new Point(c.x - a.x, c.y - a.y);
+		return ab.x * ac.y - ab.y * ac.x;
 	}
 
-	int next(int i) {
-		return i == n - 1 ? 0 : i + 1;
+	// returns if a,b,c are colinear
+	public static boolean colinear(Point a, Point b, Point c) {
+		return (a.x - b.x) * (a.y - c.y) == (a.x - c.x) * (a.y - b.y);
 	}
 
-	/* Returns 2 * area of polygon to make this aaccurate make sure your points are integer based*/
-	int area2() {
-		int area = 0;
-		for (int i = 0; i < n; i++) {
-			area += p.get(i).x * (p.get(prev(i)).y - p.get(prev(i)).y);
-		}
-		return area;
+	public static double hypot(double a, double b) {
+		return Math.sqrt(a * a + b * b);
 	}
 
-	/* Checks if a simple polygon is oriented in ccw */
-	boolean orientation() {
-		int minI = 0;
-		for (int i = 0; i < n; i++) {
-			if (p.get(i).compareTo(p.get(minI)) < 0) {
-				minI = i;
-			}
-		}
-		return cross(p.get(prev(minI)), p.get(minI), p.get(next(minI))) > 0;
+	// returns distance between points a, and b
+	public static double distance(Point a, Point b) {
+		return hypot(a.x - b.x, a.y - b.y);
 	}
+
+	// returns true iff p lies in box with opposite corners b1, b2
+	public static boolean point_in_box(Point p, Point b1, Point b2) {
+		return ((p.x >= Math.min(b1.x, b2.x)) && (p.x <= Math.max(b1.x, b2.x)) && (p.y >= Math.min(b1.y, b2.y))
+				&& (p.y <= Math.max(b1.y, b2.y)));
+	}
+
+	public static int testTriangle(double a, double b, double c) {
+		double[] tri = new double[] { a, b, c };
+		Arrays.sort(tri);
+		double smallSides = tri[0] + tri[1];
+
+		// Check degenerate case.
+		if (eq(smallSides, tri[2]))
+			return 1;
+		if (smallSides > tri[2])
+			return 2;
+		return 0;
+	}
+
+	public static boolean eq(double a, double b) {
+		return Math.abs(a - b) < EPS;
+}
 }
