c41a30f55568d91be21e7bfcab4cef035879e88e
sturdyplum <pabbelt@gmail.com>
2017-12-18 11:14:38 -0500

    Update BIT.java
    Fixed kth and removed static from methods

--- Java/DataStructures/BIT.java
+++ Java/DataStructures/BIT.java
@@ -1,41 +1,46 @@
-public class BIT {
-	private static int ft[], sum, maxi, N;
+public static class BIT {
+	private int ft[], sum, maxi, N;
+
 	BIT(int n) {
-		ft = new int[n+1];
+		ft = new int[n + 1];
 		maxi = -1;
-		while((1 << maxi) <= n+1) {
+		while ((1 << maxi) <= n + 1) {
 			maxi++;
 		}
 		maxi--;
-		n = N;
+		N = n;
 	}
-	
-	public static int rsq(int b) {
-		b++;sum = 0;
-		for(;b!=0;b -= b &-b) sum += ft[b];
+
+	public int rsq(int b) {
+		b++;
+		sum = 0;
+		for (; b != 0; b -= b & -b)
+			sum += ft[b];
 		return sum;
 	}
-	
-	public static int rsq(int a, int b) {
-		if(b < a) return 0;
-		return rsq(b) - (a==0?0:rsq(a-1));
+
+	public int rsq(int a, int b) {
+		if (b < a)
+			return 0;
+		return rsq(b) - (a == 0 ? 0 : rsq(a - 1));
 	}
-	
+
 	void adjust(int k, int v) {
 		k++;
-		for(;k < ft.length; k += k&-k) ft[k] += v;
+		for (; k < ft.length; k += k & -k)
+			ft[k] += v;
 	}
-	
+
 	int kth(int k) {
 		int ans = 0;
 		for (int i = maxi; i >= 0; --i) {
-			if(ans + (1<<i) <= N && ft[ans] + (1<<i) < k) {
-				ans += 1<<i;
+
+			if (ans + (1 << i) <= N && ft[ans + (1 << i)] <= k) {
+				ans += 1 << i;
 				k -= ft[ans];
 			}
 		}
-		return ans+1;
+		return ans;
 	}
 
 }
-
