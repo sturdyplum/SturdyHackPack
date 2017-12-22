15848dce244057957ce9ebba108a022349b66716
sturdyplum <pabbelt@gmail.com>
2017-12-22 10:15:26 -0500

    Standard fenwick tree code

--- Code/DataStructures/BIT.cpp
+++ Code/DataStructures/BIT.cpp
@@ -1,46 +1,22 @@
-/*
-  Not tested!
-*/
+// Treat as 0 indexed and it converts it to 1 indexed for you.
 struct FenwickTree
 {
     vector<int> ft;
-    FenwickTree(int n)
-    {
+    FenwickTree(int n) {
         ft.assign(n + 1, 0);
     }
-    int rsq(int b)
-    {
+    int rsq(int b) {
         b++;
         int sum = 0;
         for (; b; b -= b & -b) sum += ft[b];
         return sum;
     }
-    int rsq(int a, int b)
-    {
+    int rsq(int a, int b) {
         if(b < a) return 0;
         return rsq(b) - (a == 0 ? 0 : rsq(a - 1));
     }
-    void adjust(int k, int v)
-    {
+    void adjust(int k, int v) {
         k++;
         for (; k < ft.size(); k += k & -k) ft[k] += v;
-    }
-
-    // what this really is, is a reverse query. Meaning you ask it for a value x
-    // and it walks through the tree and finds the minimum query that would return
-    // at least x.
-    int walk(int x)
-    {
-        int number = 0;
-        for(int i = 31; i > -1; i++)
-        {
-            if((1<<i) > ft.size()) continue;
-            if(ft[number | (1 << i)] < x)
-            {
-                number |= (1<<i);
-                x -= ft[number];
-            }
-        }
-        return number;
     }
 };
